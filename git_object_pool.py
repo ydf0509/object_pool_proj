69ef391319f04eeeb87cde318b3a0ab9fa31ba9e
yangdefeng <defeng.yang@skilnets.com>
2021-06-11 16:31:25 +0800

    commit

--- git_object_pool.py
+++ git_object_pool.py
@@ -35,7 +35,7 @@ do_cmd('git commit -m commit')
 
 
 
-do_cmd('git push origin')
+do_cmd('git push')
 
 # print(subprocess.getstatusoutput('git push github'))
 print(f'{time.strftime("%H:%M:%S")}  spend_time {time.time() - t0}')
