1b7deaf127ae97cf157ea0107d117548fdd93e7e
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2022-12-05 18:02:42 +0800

    commit

--- universal_object_pool/contrib/__init__.py
+++ universal_object_pool/contrib/__init__.py
@@ -4,4 +4,4 @@ from universal_object_pool.contrib.paramiko_pool import ParamikoOperator
 from paramiko import SSHClient
 from universal_object_pool.contrib.pymysql_pool import PyMysqlOperator
 from pymysql.cursors import DictCursor
-from universal_object_pool.contrib.webdriver_pool import WebDriverOperator, PhantomJS, Chrome
+# from universal_object_pool.contrib.webdriver_pool import WebDriverOperator, PhantomJS, Chrome
